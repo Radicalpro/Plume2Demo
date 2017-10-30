d5b8bb18d9fa85791ed699084985890673d933c0
tianyi <793187547@qq.com>
2017-10-30 12:57:46 +0800

    flatList基础公共组件

---
+++ ajkit/flatList/index.js
@@ -0,0 +1,92 @@
+/**
+ * FlatList组件
+ */
+import React, {Component} from 'react'
+import {FlatList, RefreshControl} from 'react-native'
+
+export default class AJFlatList extends Component {
+
+	static defaultProps = {
+		//默认页数
+		pageSize: 10,
+		//当前的数据
+		dataSource: [],
+		//传入数组数据的唯一性id
+		keyProps: 'id',
+		renderRow: null,
+		//列表为空是组件
+		ListEmptyComponent: null,
+		//尾部组件
+		ListFooterComponent: null,
+		//头部组件
+		ListHeaderComponent: null,
+		//上拉加载方法
+		onEndReached: null,
+		//下拉刷新方法
+		onRefresh: null,
+		//多余的参数，state等变量
+		extraData: {},
+	};
+
+	constructor(props) {
+		super(props);
+		const {dataSource} = this.props;
+		this.state = {
+			refreshing: false,
+			//当前的数据源
+			dataSource: dataSource || []
+		}
+	}
+
+	render() {
+		const {
+			pageSize,
+			renderRow,
+			ListEmptyComponent,
+			extraData,
+			ListFooterComponent,
+			ListHeaderComponent,
+			onEndReached,
+			onRefresh,
+			keyProps
+		} = this.props;
+
+		//如果数据为空
+		let {dataSource} = this.state;
+
+		//如果数据不为空
+		return (
+			<FlatList
+				ref="listRef"
+				data={dataSource}
+				renderItem={({item, index}) => renderRow(item, extraData, index)}
+				//水平或垂直展示
+				horizontal={false}
+				//初始化渲染行数
+				initialNumToRender={pageSize}
+				//此函数用于为给定的item生成一个不重复的key
+				keyExtractor={(item) => {
+					return item[keyProps]
+				}}
+				//0.5表示距离内容最底部的距离为当前列表可见长度的一半时触发。
+				onEndReachedThreshold={0.3}
+				//上拉加载
+				onEndReached={onEndReached && onEndReached()}
+				//底部组件
+				ListFooterComponent={ListFooterComponent && ListFooterComponent()}
+				//头部组价
+				ListHeaderComponent={ListHeaderComponent && ListHeaderComponent()}
+				//列表为空组件
+				ListEmptyComponent={ListEmptyComponent && ListEmptyComponent()}
+				//如果有除data以外的数据用在列表中
+				extraData={extraData}
+				refreshControl={
+					<RefreshControl
+						refreshing={this.state.refreshing}
+						onRefresh={onRefresh && onRefresh()}
+					/>
+				}
+			/>
+		)
+	}
+}
